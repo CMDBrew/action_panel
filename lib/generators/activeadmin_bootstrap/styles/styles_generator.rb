2323b38adb60431b49b443a86959a483ce24c625
Nelson Lee <nelson@cmdbrew.com>
2019-09-27 12:03:55 -0700

    Update style_generator

--- lib/generators/activeadmin_bootstrap/styles/styles_generator.rb
+++ lib/generators/activeadmin_bootstrap/styles/styles_generator.rb
@@ -11,23 +11,23 @@ module ActiveadminBootstrap
       )
 
       def setup_directory
-        empty_directory 'app/assets/stylesheets/activeadmin_bootstrap_overrides'
+        empty_directory 'app/assets/stylesheets/activeadmin_bootstrap'
       end
 
       def copy_vars
         template(
           '_bootstrap_vars.scss',
-          'app/assets/stylesheets/activeadmin_bootstrap_overrides/_bootstrap_vars.scss'
+          'app/assets/stylesheets/activeadmin_bootstrap/_bootstrap_vars.scss'
         )
         template(
           '_vars.scss',
-          'app/assets/stylesheets/activeadmin_bootstrap_overrides/_vars.scss'
+          'app/assets/stylesheets/activeadmin_bootstrap/_vars.scss'
         )
       end
 
       def setup_imports
         inject_into_file 'app/assets/stylesheets/active_admin.scss',
-                         "\n@import \'activeadmin_bootstrap_overrides/**/*\';",
+                         "\n@import \'activeadmin_bootstrap/**/*\';",
                          after: %r{// Active Admin's got SASS!}
       end
 
