826786351bb30aa7954a560a673cf3f9fed4001e
Nelson Lee <ilung.me@gmail.com>
2020-05-07 10:04:07 -0700

    Fix component styles

--- lib/activeadmin_bootstrap/extensions/formtastic/form_builder.rb
+++ lib/activeadmin_bootstrap/extensions/formtastic/form_builder.rb
@@ -3,9 +3,9 @@ module Formtastic
   # Overwrite FormBuilder - formtastic/lib/formtastic/form_builder.rb
   class FormBuilder < ActionView::Helpers::FormBuilder
 
-    configure :default_inline_error_class, 'small mb-0 text-danger'
+    configure :default_inline_error_class, 'small mb-0 mt-2 text-danger'
     configure :default_error_list_class, 'errors'
-    configure :default_hint_class, 'small mb-0 text-secondary'
+    configure :default_hint_class, 'small mb-0 mt-2 text-secondary'
 
   end
 
